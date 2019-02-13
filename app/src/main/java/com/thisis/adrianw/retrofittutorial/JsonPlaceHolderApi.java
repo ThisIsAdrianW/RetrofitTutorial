c284c09ac73c40ca051637552a5a3a53fa0585f6
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-02-13 02:01:36 +0100

    Updated Call interface Added FindViewById in MainActivity for only TextView

--- app/src/main/java/com/thisis/adrianw/retrofittutorial/JsonPlaceHolderApi.java
+++ app/src/main/java/com/thisis/adrianw/retrofittutorial/JsonPlaceHolderApi.java
@@ -2,7 +2,9 @@ package com.thisis.adrianw.retrofittutorial;
 
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 public interface JsonPlaceHolderApi {
+    @GET("posts")
     Call<List<Post>> getPosts();
 }
