8d43c4d464b9d3381bc98dcd86167259bc6df099
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-02-13 03:35:44 +0100

    Added variables for comment class

--- app/src/main/java/com/thisis/adrianw/retrofittutorial/Comment.java
+++ app/src/main/java/com/thisis/adrianw/retrofittutorial/Comment.java
@@ -1,4 +1,14 @@
 package com.thisis.adrianw.retrofittutorial;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Comment {
+    private int postID;
+    private int id;
+    private String name;
+    private String email;
+    @SerializedName("body")
+    private String text;
+
+    
 }
