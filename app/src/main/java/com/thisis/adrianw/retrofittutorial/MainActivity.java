4b1e1c4ffa1eaab5fb66db757d886aa16e454a6f
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-02-13 02:23:03 +0100

    Updated gradle Created retrofit bulider in MainActivity

--- app/src/main/java/com/thisis/adrianw/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/thisis/adrianw/retrofittutorial/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
 
@@ -13,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResult = findViewById(R.id.text_view_result);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
