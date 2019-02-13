74535df0b9374984680fc6b6a7054807d31a46f2
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-02-13 03:33:43 +0100

    Added comment class Updated interface with call for comments

---
+++ app/src/main/java/com/thisis/adrianw/retrofittutorial/Comment.java
@@ -0,0 +1,4 @@
+package com.thisis.adrianw.retrofittutorial;
+
+public class Comment {
+}
--- app/src/main/java/com/thisis/adrianw/retrofittutorial/JsonPlaceHolderApi.java
+++ app/src/main/java/com/thisis/adrianw/retrofittutorial/JsonPlaceHolderApi.java
@@ -7,4 +7,7 @@ import retrofit2.http.GET;
 public interface JsonPlaceHolderApi {
     @GET("posts")
     Call<List<Post>> getPosts();
+
+    @GET("posts/2/comments")
+    Call<List<Comment>> getComments();
 }
